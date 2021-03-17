7146375150a8ca73aff54f8c90c9feb1ae05bfa3
Josh Bengtson <josh.bengtson@gmail.com>
2021-03-17 11:18:46 -0400

    first commit

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using TicTacToe;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the size of the board: ");
+            // I AM ASSUMING YOU ONLY ENTER AN INTEGER.  NORMAL TYPE CHECKING WOULD BE NEEDED IN THE REAL WORLD.
+            int n = int.Parse(Console.ReadLine());
+            TicTacToe game = new TicTacToe(n);
+
+            Console.WriteLine("Welcome to tic tac toe.");
+            Console.WriteLine("Your entries must be in the form of 'x,y'.");
+            //I ASSUME THAT YOU DON'T TRY AND BREAK THE GAME AS I DID NOT PUT OUT OF BOUNDS CHECKS IN.  THIS WASNT DONE TO SAVING TIME.
+            //the board starts at 0,0 and goes to n-1,n-1 - yes I could pretty this up to make it easier but for sake of proving it works i left it as is.
+
+            int player = 2;  //i will flip this to 1 to start in the do loop.
+            int win = 0;
+
+            do
+            {
+                player = game.getPlayer(player);  //swap the player to the next position
+                Console.WriteLine($"Player {player}, please enter your move:");
+                string move = Console.ReadLine();
+
+                win = game.PlacePiece(int.Parse(move.Substring(0, 1)), int.Parse(move.Substring(2, 1)), player);
+            } while (win == 0);
+
+            if (win != 3)
+            {
+                Console.WriteLine($"Player {player} won.");
+            }
+            else
+            {
+                Console.WriteLine($"It was a draw.");
+            }
+
+        }
+    }
+}
---
+++ TicTacToe/TicTacToe.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class TicTacToe
+    {
+        private int moveCount;
+        private int[,] board;
+        private int size;
+        /// Created a Tic Tac Tow game board
+        /// <param name="boardSize">board is of size nxn</param>
+        public TicTacToe(int n)
+        {
+            board = new int[n, n];  ///new array is initialized with 0's
+            moveCount = 0;
+            size = n;
+        }
+
+        /// &lt;summary&gt;
+        /// Place a piece on the game board
+        /// &lt;/summary&gt;
+        /// &lt;param name=&quot;row&quot;&gt;row to place a piece&lt;/param&gt;
+        /// &lt;param name=&quot;col&quot;&gt;column to place a piece&lt;/param&gt;
+        /// &lt;param name=&quot;player&quot;&gt;the player (1 or 2) the piece is for&lt;/param&gt;
+        /// &lt;returns&gt;0 = no winner, 1 = player 1 won, 2 = player 2 won&lt;/returns&gt;
+        public int PlacePiece(int row, int col, int player)
+        {
+            //player = 1 or 2
+
+            if (board[row,col] == 0)
+            {
+                board[row,col] = player;
+            }
+            moveCount++;
+
+            //check the row
+            for (int a = 0; a < size; a++)
+            {
+                //only need to check the row which the player guessed
+                if (board[a, col] != player)
+                {
+                    //found a point for the other player or empty
+                    break;
+                }
+
+                if (a == size - 1)
+                {
+                    return player;
+                }
+            }
+
+            //check the col, and only need to check for what they just guessed
+            for (int a = 0; a < size; a++)
+            {
+                if (board[row,a] != player)
+                {
+                    //found a point for the other player or empty
+                    break;
+                }
+                if (a == size - 1)
+                {
+                    return player;
+                }
+            }
+
+            //check diagonal   /
+            if (row == col)
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    if (board[i, i] != player)
+                    {
+                        //found a no match
+                        break;
+                    }
+                    if (i == size - 1)
+                    {
+                        return player;
+                    }
+                }
+            }
+
+            //check other diag    \
+            if (row + col == size - 1)
+            {
+                for (int a = 0; a < size; a++)
+                {
+                    if (board[a, (size - 1) - a] != player)
+                    {
+                        break;
+                    }
+                    if (a == size - 1)
+                    {
+                        return player;
+                    }
+                }
+            }
+
+            //draw - no more moves
+            if (moveCount == Math.Pow(size, 2))
+            {
+                //I added a pure draw condition as it wasnt in the description but it can happen.
+                return 3;
+            }
+
+            return 0;
+        }
+
+        public int getPlayer(int p)
+        {
+            if (p == 1) return 2;
+            if (p == 2) return 1;
+            return 0;
+        }
+    }
+}
